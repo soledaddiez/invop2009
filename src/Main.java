eb68abd5b6d4908276f0fb17dd74ef89ef2c0588
sole10g <sole10g@07581a52-e203-11de-9fd5-4be1c9d5bc18>
2009-12-07 01:55:23 +0000

    Agregado de la GUI

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
+import gui.PedidosPanel;
+import gui.VerDemanda;
 
-
+import java.awt.Dimension;
 import java.util.List;
+
+import javax.swing.JFrame;
+import javax.swing.JTabbedPane;
+import javax.swing.WindowConstants;
 
 import modelo.Pedido;
 import modelo.Producto;
+
 import dbConnector.PedidoManager;
 import dbConnector.ProductoManager;
 
@@ -29,14 +36,40 @@ public class Main {
 			e.printStackTrace();
 		}
 		
-		PedidoManager pm = new PedidoManager();
-		Pedido pedido = new Pedido();
-		pedido.setId(3);
-		try {
-			pm.savePedido(pedido);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+//		PedidoManager pm = new PedidoManager();
+//		Pedido pedido = new Pedido();
+//		pedido.setId(3);
+//		try {
+//			pm.savePedido(pedido);
+//		} catch (Exception e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
 	}
+	
+	//Este METODO mail llama a la GUI...
+//	public static void main(String[] args){
+//		try {
+//			ProductoManager productoManager = new ProductoManager();
+//			
+//			JFrame frame = new JFrame();
+//			PedidosPanel pedidos = new PedidosPanel(productoManager.getListadoDeProductos());
+//			JTabbedPane principal = new JTabbedPane();
+//			principal.addTab("Nuevo Pedido", pedidos);
+//			principal.setTabPlacement(JTabbedPane.LEFT);
+//			double valores[] = {10,2,3,4,5,6,7,8,9,10,11,12};
+//			String meses[] = {"1","2","3","4","5","6","7","8","9","10","11","12"};
+//			principal.addTab("Demanda", new VerDemanda(valores, meses, "Demanda anual"));
+//			frame.setPreferredSize(new Dimension(600, 500));
+//			frame.setContentPane(principal);
+//			frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+//			frame.pack();
+//			frame.setVisible(true);
+//		} catch (Exception e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		
+//	
+//	}
 }
