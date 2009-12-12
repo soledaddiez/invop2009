55022e35b10e1747748ca4ad5fd9a365dc612400
sole10g <sole10g@07581a52-e203-11de-9fd5-4be1c9d5bc18>
2009-12-12 03:30:05 +0000

    * findByCriteria en el GenericDAO * getTasaFabricacion en TasaFabricacionDAO * LineaDAO * corrección del init para la tabla Producto (falta todo lo demás)

--- src/dao/impl/TasaProduccionDAO.java
+++ src/dao/impl/TasaProduccionDAO.java
@@ -1,5 +1,8 @@
 package dao.impl;
 
+import java.util.Hashtable;
+import java.util.List;
+
 import dao.GenericDAO;
 import modelo.Linea;
 import modelo.Producto;
@@ -13,9 +16,19 @@ public class TasaProduccionDAO extends GenericDAO<TasaProduccion>{
 	 * @param producto
 	 * @return Tasa de Produccion
 	 * @author Soledad Diez
+	 * @return 
 	 */
 	public TasaProduccion getTasaProduccion(Linea linea, Producto producto){
-		//TODO Este método tendría que recuperar de la bd la TasaProduccion
+		Hashtable<String, Object> criteria = new Hashtable<String, Object>();
+		criteria.put("linea", linea);
+		criteria.put("producto", producto);
+		
+		List<TasaProduccion> tasaProd = this.findByCriteria(criteria);
+		
+		if ((tasaProd != null) &&(tasaProd.size()>0))
+			return tasaProd.get(0);
+//		return null;
+		//TODO (ELIMINAR)Lo dejo de prueba mientras no esten los valores reales en la base de la tasa de produccion
 		return new TasaProduccion(new Long(1), new Long (5000), linea, producto) ;
 	}
 	
