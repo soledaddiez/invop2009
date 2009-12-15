6aaf5e21c479a5854618748a3a3720d07055cc76
sole10g <sole10g@07581a52-e203-11de-9fd5-4be1c9d5bc18>
2009-12-15 17:48:50 +0000

    Consulta Demandas a partir de una fecha.

--- src/dao/impl/PedidoDAO.java
+++ src/dao/impl/PedidoDAO.java
@@ -1,8 +1,62 @@
 package dao.impl;
 
-import dao.GenericDAO;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.Hashtable;
+import java.util.List;
+
+import modelo.Demanda;
 import modelo.Pedido;
+
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Restrictions;
+
+import dao.GenericDAO;
 
 public class PedidoDAO extends GenericDAO<Pedido>{
 
+	public List<Demanda> getDemandas(Timestamp fechaInicio){
+		List<Demanda> demandas = new ArrayList<Demanda>();
+
+		List<Pedido> pedidos = this.getPedidosDesdeFecha(fechaInicio);
+		
+		Hashtable<String, Demanda> hash = new Hashtable<String, Demanda>();
+		Demanda demanda;
+
+		for(Pedido pedido : pedidos){
+			
+			if(hash.containsKey(pedido.getProducto().getId()+"-"+pedido.getFechaOrden().toString())){
+				demanda = hash.get(pedido.getProducto().getId()+"-"+pedido.getFechaOrden().toString());
+				demanda.setCantidad(demanda.getCantidad()+pedido.getCantidad());
+				hash.put(pedido.getProducto().getId()+"-"+pedido.getFechaOrden().toString(), demanda);
+			}
+			else{
+				demanda = new Demanda(pedido.getProducto(), pedido.getCantidad(), pedido.getFechaOrden());
+				hash.put(pedido.getProducto().getId()+"-"+pedido.getFechaOrden().toString(), demanda);
+			}
+		}
+		
+		for(Enumeration en=hash.keys(); en.hasMoreElements(); ){
+			demandas.add(hash.get(en.nextElement()));
+		}
+
+		return demandas;
+	}
+	
+	/**
+	 * Obtiene todos los pedidos a partir de una fecha dada.
+	 * @param fechaInicio
+	 * @return listado de pedidos existentes.
+	 */
+	public List<Pedido> getPedidosDesdeFecha(Timestamp fechaInicio){
+		Criteria crit = getHibernateTemplate().createCriteria(domainClass.getName());
+		if (fechaInicio != null){
+			crit.add(Restrictions.ge("fechaOrden", fechaInicio));	
+		}
+		
+		List<Pedido> returnList = crit.list(); 
+		session.getTransaction().commit();
+		return returnList;
+	}
 }
