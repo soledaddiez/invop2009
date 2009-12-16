e3b145e70a0d6d61a8077a4385f3f852ec0f33af
sole10g <sole10g@07581a52-e203-11de-9fd5-4be1c9d5bc18>
2009-12-16 22:56:39 +0000

    Creación de Cliente y su asociación con Pedido

---
+++ src/dao/impl/ClienteDAO.java
@@ -0,0 +1,14 @@
+package dao.impl;
+
+import dao.GenericDAO;
+import modelo.Cliente;
+
+/**
+ * Clase que define los métodos para manejo de Clientes 
+ * @author Soledad Diez
+ */
+
+public class ClienteDAO extends GenericDAO<Cliente>{
+
+}
+
