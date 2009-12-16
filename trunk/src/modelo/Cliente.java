1d940a8325deffa32d21e5ce69d4be3d7e27ba09
sole10g <sole10g@07581a52-e203-11de-9fd5-4be1c9d5bc18>
2009-12-16 22:54:10 +0000

    Creación de Cliente y su asociación con Pedido

---
+++ trunk/src/modelo/Cliente.java
@@ -0,0 +1,60 @@
+package modelo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table (name="CLIENTE")
+public class Cliente {
+
+	private Long id;
+	private String nombre;
+	private String apellido;
+	private String direccion;
+	private String telefono;
+	
+	@Id
+    @GeneratedValue
+    @Column(name = "id")
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	@Column(name = "nombre")
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	@Column(name = "apellido")
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	
+	@Column(name = "direccion")
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	
+	@Column(name = "telefono")
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}	
+}
